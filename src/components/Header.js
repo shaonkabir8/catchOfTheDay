411cd94e8fd332ef2f9364a5619f83cc8b279dc0
unknown <shaonkabir98@gmail.com>
2019-08-06 09:14:29 -0700

    Configuring my React Application for working

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+class Header extends Component{
+    render() {
+        return(
+            <div>
+                <h1>{this.props.title}</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
