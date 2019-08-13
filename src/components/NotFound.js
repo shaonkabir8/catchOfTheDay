3b523dfa2d5e2e4fff6072dc51d5dd56116d3e9b
unknown <shaonkabir98@gmail.com>
2019-08-13 03:36:44 -0700

    Not Found page design Updated

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,7 @@ import React from 'react'
 function NotFound({location}) {
     return (
         <div className="not-found">
-            <h1>No Page Found as {location.pathname}</h1>
+            <h1>No Page Found as <span>{location.pathname}</span></h1>
         </div>
     )
 }
