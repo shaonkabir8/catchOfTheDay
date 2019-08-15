7bc764c1ec7281596a12ec281ce4f8dd299b4117
unknown <shaonkabir98@gmail.com>
2019-08-15 06:35:09 -0700

    Path component deleted and moved all routes to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import {render} from 'react-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './css/style.css'
+import App from './components/App'
+import StorePicker from './components/StorePicker'
+import NotFound from './components/NotFound';
+import FirstPage from './components/FirstPage';
 
-import Path from './components/Path'
+const repo = `/${window.location.pathname.split('/')[1]}`
 
+const Path = () => {
+    return(
+        <Router basename={repo}>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={StorePicker}/>
+                    <Route path="/store/:storeId" component={FirstPage} />
+                    <Route path="/item/:storeId" component={App} />
+                    <Route  component={NotFound}/>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
 render(<Path/> , document.querySelector('#root'))
